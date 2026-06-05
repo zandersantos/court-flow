aea0aaad37b7c4ae0b9935dac4c4c786410308f7
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-06-05 12:09:59 -0500

    Added a View Playoffs button that redirects the user to the playoffs page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-8 p-8">
       <GameScore />
-    </main>
 
+      <a href="/playoffs" className="text-sm underline text-gray-500 hover:text-black">
+        View All Playoffs
+      </a>
+    </main>
   );
 }
