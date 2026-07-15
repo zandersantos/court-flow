a22b40bc3cda650cd04451d7f101596884067eaa
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-07-14 22:12:02 -0500

    Updated hero and game card gradients, updated font sizing, replaced text-md with text-base, and updated gaps and spacing

--- components/season/Seasons.tsx
+++ components/season/Seasons.tsx
@@ -65,7 +65,7 @@ export default function Seasons({ games }: SeasonsProps) {
 
   return (
     <div className="min-h-screen px-8 py-12 bg-white text-black">
-      <div className="relative overflow-hidden rounded-3xl bg-black text-white px-10 py-12 mb-10 shadow-xl bg-gradient-to-br from-yellow-400/30 via-transparent to-transparent">
+      <div className="relative overflow-hidden rounded-3xl text-white px-10 py-12 mb-10 bg-gradient-to-br from-yellow-400/50 via-black to-black text-white rounded-3xl px-8 py-7 shadow-xl hover:-translate-y-1 hover:shadow-2xl transition-all shadow-xl">
         <div className="relative flex items-start justify-between">
           <p className="text-4xl text-yellow-500 uppercase tracking-widest font-bold">
             CourtFlow
@@ -73,7 +73,7 @@ export default function Seasons({ games }: SeasonsProps) {
 
           <Link
             href="/"
-            className="inline-flex items-center uppercase tracking-widest gap-2 px-8 py-3 border border-black font-bold bg-yellow-400 text-black font-bold text-md rounded-full transition-all duration-200 hover:bg-yellow-400/10 hover:border-yellow-400 hover:text-yellow-400"
+            className="inline-flex items-center uppercase tracking-widest gap-2 px-8 py-3 border border-black font-bold bg-yellow-400 text-black text-md rounded-full transition-all duration-200 hover:bg-yellow-400/10 hover:border-yellow-400 hover:text-yellow-400"
           >
             Home
           </Link>
@@ -86,15 +86,15 @@ export default function Seasons({ games }: SeasonsProps) {
       </div>
 
       <div className="rounded-3xl p-6 mb-10 bg-gray-50 border border-gray-200 shadow-sm">
-        <p className="text-md uppercase tracking-widest font-semibold text-black mb-4">
+        <p className="text-base uppercase tracking-widest font-semibold text-black mb-4">
           Game Type
         </p>
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex gap-5 flex-wrap">
           {gameTypes.map((type) => (
             <button
               key={type}
               onClick={() => handleFilter("gameType", type)}
-              className={`px-4 py-1 rounded-full font-semibold border transition
+              className={`px-4 py-2 rounded-full font-semibold border transition
                 ${
                   selectedType === type
                   ? "bg-black text-white border-black"
@@ -108,7 +108,7 @@ export default function Seasons({ games }: SeasonsProps) {
       </div>
 
       <div className="rounded-3xl p-6 mb-10 bg-gray-50 border border-gray-200 shadow-sm">
-        <p className="text-md uppercase tracking-widest font-semibold text-black mb-4">
+        <p className="text-base uppercase tracking-widest font-semibold text-black mb-4">
           Month
         </p>
         <div className="grid grid-cols-2 sm:grid-cols-5 lg:grid-cols-10 gap-3">
@@ -116,7 +116,7 @@ export default function Seasons({ games }: SeasonsProps) {
             <button
               key={month}
               onClick={() => handleFilter("month", month)}
-              className={`px-4 py-1 rounded-full font-semibold border transition
+              className={`px-4 py-2 rounded-full font-semibold border transition
                 ${
                   selectedMonth === month
                   ? "bg-black text-white border-black"
@@ -129,13 +129,13 @@ export default function Seasons({ games }: SeasonsProps) {
         </div>
       </div>
 
-      <p className="text-md text-gray-600 uppercase tracking-widest mb-2">
+      <p className="text-base text-gray-600 uppercase tracking-widest mb-2">
         {filtered.length} Total Games
       </p>
 
       <div className="flex flex-col gap-2">
         {paginated.map((game) => (
-          <div key ={game.id} className="bg-black text-white rounded-3xl px-8 py-7 bg-gradient-to-br from-yellow-400/30 via-transparent to-transparent">
+          <div key ={game.id} className="bg-gradient-to-br from-yellow-400/50 via-black to-black text-white rounded-3xl px-8 py-7 shadow-xl hover:-translate-y-1 hover:shadow-2xl transition-all shadow-xl">
             <div className="flex items-center justify-between mb-8">
               <span className="text-lg text-white uppercase font-semibold tracking-widest">
                 {new Date(game.date).toLocaleDateString("en-US", {
@@ -145,14 +145,14 @@ export default function Seasons({ games }: SeasonsProps) {
                 })}
               </span>
 
-              <span className="bg-yellow-400 text-black px-4 py-1 rounded-full text-md font-black">
+              <span className="bg-yellow-400 text-black px-4 py-1 rounded-full text-base font-black">
                 FINAL
               </span>
             </div>
 
             <div className="flex items-center justify-between mb-4">
               <div className="flex items-center gap-3">
-                <span className={`text-3xl font-semibold ${game.winnerName === game.homeTeam.fullName ? "text-yellow-500" : "text-white"}`}>
+                <span className={`text-xl font-semibold ${game.winnerName === game.homeTeam.fullName ? "text-yellow-500" : "text-white"}`}>
                   {game.homeTeam.abbreviation}
                 </span>
                 <span className={`text-3xl font-semibold ${game.winnerName === game.homeTeam.fullName ? "text-yellow-500" : "text-white"}`}>
@@ -165,7 +165,7 @@ export default function Seasons({ games }: SeasonsProps) {
             </div>
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-3">
-                <span className={`text-3xl font-semibold ${game.winnerName === game.visitorTeam.fullName ? "text-yellow-500" : "text-white"}`}>
+                <span className={`text-xl font-semibold ${game.winnerName === game.visitorTeam.fullName ? "text-yellow-500" : "text-white"}`}>
                   {game.visitorTeam.abbreviation}
                 </span>
                 <span className={`text-3xl font-semibold ${game.winnerName === game.visitorTeam.fullName ? "text-yellow-500" : "text-white"}`}>
@@ -181,21 +181,21 @@ export default function Seasons({ games }: SeasonsProps) {
         ))}
       </div>
       {totalPages > 1 && (
-        <div className="flex items-center justify-center gap-3 mt-10">
+        <div className="flex items-center justify-center gap-5 mt-12">
           <button
             onClick={() => setPage((p) => Math.max(1, p-1))}
             disabled={page===1}
-            className="px-4 py-2 text-md font=semibold border boder-gray-300 rounded-full hover:border-black disabled:opacity-30 disabled:cursor-not-allowed transition-all"
+            className="px-4 py-2 text-base font-semibold border border-gray-300 rounded-full hover:border-black disabled:opacity-30 disabled:cursor-not-allowed"
           >
             - Previous
           </button>
-          <span className="text-md text-gray-400">
+          <span className="text-base text-gray-400">
             Page {page} of {totalPages}
           </span>
           <button
             onClick={() => setPage((p) => Math.min(totalPages, p+1))}
             disabled={page === totalPages}
-            className="px-4 py-2 text-md font=semibold border boder-gray-300 rounded-full hover:border-black disabled:opacity-30 disabled:cursor-not-allowed transition-all"
+            className="px-4 py-2 text-base font-semibold border border-gray-300 rounded-full hover:border-black disabled:opacity-30 disabled:cursor-not-allowed"
           >
             Next -
           </button>
