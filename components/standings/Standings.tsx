93354989a591844643511520cb021cf66f882464
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-07-20 13:58:20 -0500

    Added season header layout to standings page

--- components/standings/Standings.tsx
+++ components/standings/Standings.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from "next/link"
+
 type Team = {
   id: number
   fullName: string
@@ -75,12 +77,32 @@ export default function Standings({ games }: StandingsProps) {
   )
 
   return (
-    <div>
-      {sortedStandings.map((standing) =>
-        <p key={standing.team.id}>
-          {standing.team.fullName} {standing.wins}-{standing.losses}
-        </p>
-      )}
+    <div className="min-h-screen px-8 py-12 bg-white text-black">
+      <div className="relative overflow-hidden rounded-3xl text-white px-10 py-12 mb-10 bg-gradient-to-br from-yellow-400/50 via-black to-black hover:shadow-2xl transition-all shadow-xl">
+        <div className="relative flex items-start justify-between">
+          <p className="text-4xl text-yellow-500 uppercase tracking-widest font-bold">
+            CourtFlow
+          </p>
+
+          <Link
+            href="/"
+            className="inline-flex items-center uppercase tracking-widest gap-2 px-8 py-3 border border-black font-bold bg-yellow-400 text-black text-md rounded-full transition-all duration-200 hover:bg-yellow-400/10 hover:border-yellow-400 hover:text-yellow-400"
+          >
+            Home
+          </Link>
+        </div>
+
+        <h1 className="text-2xl font-black uppercase mt-4 tracking-widest">
+          2025-2026 NBA Season
+        </h1>
+      </div>
+      <div>
+        {sortedStandings.map((standing) =>
+          <p key={standing.team.id}>
+            {standing.team.fullName} {standing.wins}-{standing.losses}
+          </p>
+        )}
+        </div>
     </div>
   )
 
